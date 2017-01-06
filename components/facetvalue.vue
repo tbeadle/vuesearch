bb9d6ca5c3da6ec824595783fccdf006913293be
Tommy Beadle <tbeadle@gmail.com>
2017-01-06 14:34:48 -0500

    Add beginning of facetvalue component.

---
+++ components/facetvalue.vue
@@ -0,0 +1,33 @@
+<template>
+	<ul class='dropdown-menu'>
+		<li v-for='item in items'><a href='#'>{{ item }}</a></li>
+	</ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+	props: [
+		'facet'
+	],
+	data: function() {
+		return {
+			items: [],
+		}
+	},
+	created: function() {
+		axios.get(`/api/facetvalues/${this.facet}.json`)
+			.then(
+				(response) => {
+					this.items = response.data;
+				}
+			)
+			.catch(
+				(response) => {
+					this.items = [];
+				}
+			);
+	},
+}
+</script>
