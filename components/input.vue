5ab3231a1d1509897616c2ec7ed4c5ee80cecfe8
Tommy Beadle <tbeadle@gmail.com>
2017-01-06 16:19:21 -0500

    Super basic input and search components.

---
+++ components/input.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class='form-group has-feedback'>
+		<label class='control-label sr-only'>Search</label>
+		<input class='form-control VS-input'>
+		<span class='form-control-feedback glyphicon glyphicon-search'>
+	</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.VS-input {
+	/* Half of .form-control -> height */
+	border-radius: 17px;
+}
+</style>
---
+++ components/search.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class='VS-search'>
+		<vs-input></vs-input>
+	</div>
+</template>
+
+<script>
+import input from './input.vue';
+
+export default {
+	components: {
+		'vs-input': input,
+	},
+	props: [
+		'config',
+	],
+}
+</script>
